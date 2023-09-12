53047d051d7d0af03e14eeb0596b4ab356e759bd
Kausalya <kausalya@qualitythought.in>
2023-09-12 10:38:31 +0530

    This program belogs to Addition

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package javaprograms;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		MethodExplanation me= new MethodExplanation();
+		me.MethodExplanation();
+		me.MethodExplan();
+	}
+
+}
