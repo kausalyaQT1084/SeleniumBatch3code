d9969dedb0fd53558eb98ff63ec98ca47cba84ab
Kausalya <kausalya@qualitythought.in>
2023-09-12 10:47:27 +0530

    Swapping prg

---
+++ Swapingexample.java
@@ -0,0 +1,20 @@
+package javaprograms;
+
+public class Swapingexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Swapping use thrid variable
+		int i=10;
+		int j=20;
+		
+		
+		i=j;//20
+		j=i;
+		System.out.println(i);
+		System.out.println(j);
+				
+
+	}
+
+}
